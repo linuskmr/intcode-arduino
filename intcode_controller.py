2be8f61f0bfb16d32bedc99f75c10c26acd1a59f
linus-k519 <56581587+linus-k519@users.noreply.github.com>
2020-12-27 13:45:53 +0100

    Added comments

--- intcode_controller.py
+++ intcode_controller.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3.8
 
-import sys
 from time import time
 import serial
 from typing import List
@@ -8,63 +7,64 @@ import argparse
 import re
 
 
-def debug(*args):
-    print('\033[34m', end='')
-    print(*args, end='')
-    print('\033[0m')
-
-
-class Program(list):
+# A program is an automatically growing list of integers and can apply commands from the Arduino to this list using
+# exec().
+class Program(List[int]):
     finish = False
 
+    @classmethod
+    def from_text(cls, text: str):
+        # Remove comments
+        text = re.sub('\\s*#.*', '', text)
+
+        # Convert spaces and newlines to comma
+        text = text.replace('\n', ',')
+        text = text.replace(' ', ',')
+
+        # Remove multiple commas
+        text = re.sub(r',+', ',', text)
+
+        # Remove possible empty first element
+        text = re.sub(r'^,', '', text)
+
+        # Split numbers by comma and parse them to integers
+        comma_separated = text.split(',')
+        comma_separated_int = [int(x) for x in comma_separated]
+        return cls(comma_separated_int)
+
+    # Gets a value at the given position. The list grows if necessary to this index.
     def __getitem__(self, index):
         if index >= len(self):
             self.extend([0] * (index + 1 - len(self)))
         return list.__getitem__(self, index)
 
+    # Sets a value at the given position. The list grows if necessary to this index.
     def __setitem__(self, index, value):
         if index >= len(self):
             self.extend([0] * (index + 1 - len(self)))
         list.__setitem__(self, index, value)
 
+    # Executes a command from the arduino on this list.
     def exec(self, cmd: List[str]):
         operation = cmd[0]
-        args = cmd[1:]
         if operation == "GET":
-            val = self[int(args[0])]
-            return val
+            # Get the value at position
+            return self[int(cmd[1])]
         elif operation == "SET":
-            self[int(args[0])] = int(args[1])
+            # Set the value at the position to the value
+            self[int(cmd[1])] = int(cmd[2])
         elif operation == "INPUT":
-            val = input("INPUT ")
-            return val
+            # Asks the user for an input
+            return input("INPUT ")
         elif operation == "OUTPUT":
-            print('OUTPUT', int(args[0]))
+            # Outputs a value from the arduino
+            print('OUTPUT', int(cmd[1]))
         elif operation == "HALT" or operation == "ERROR":
+            # Ends the execution of the program
             print(*cmd)
             self.finish = True
         else:
-            debug(*cmd)
-
-
-def str_to_comma_separated(text: str):
-    # Remove comments
-    text = re.sub('\\s*#.*', '', text)
-
-    # Convert spaces and newlines to comma
-    text = text.replace('\n', ',')
-    text = text.replace(' ', ',')
-
-    # Remove multiple commas
-    text = re.sub(r',+', ',', text)
-
-    # Remove possible empty first element
-    text = re.sub(r'^,', '', text)
-
-    # Split numbers by comma and parse them to integers
-    comma_separated = text.split(',')
-    comma_separated_int = [int(x) for x in comma_separated]
-    return comma_separated_int
+            print(*cmd)
 
 
 if __name__ == '__main__':
@@ -74,24 +74,26 @@ if __name__ == '__main__':
     parser.add_argument('file', type=str, help='Path to program, which should be executed')
     args = parser.parse_args()
 
-    ard = serial.Serial(args.port, args.baud, timeout=5)
+    arduino = serial.Serial(args.port, args.baud, timeout=5)
 
-    fileContent = ''
+    file_content = ''
     with open(args.file) as file:
-        fileContent = file.read()
+        file_content = file.read()
 
-    program = Program([int(x) for x in fileContent.split(',')])
-
+    program = Program.from_text(file_content)
     start_time = time()
-    while True:
-        instruction = ard.readline().decode().replace('\r\n', '')
+
+    # Run program
+    while not program.finish:
+        # Read a instruction from the arduino
+        instruction = arduino.readline().decode().replace('\r\n', '')
         if not instruction:
             continue
         instruction = instruction.split()
         data = program.exec(instruction)
-        if program.finish:
-            end_time = time()
-            print(f'Execution took {end_time - start_time:.2f}s')
-            exit()
         if data is not None:
-            ard.write((str(data) + '\n').encode())
+            # Write return value from program.exec() to arduino
+            arduino.write((str(data) + '\n').encode())
+
+    end_time = time()
+    print(f'Execution took {end_time - start_time:.2f}s')
